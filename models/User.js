b32dccf83db3aec953776a8873d69288f92e4295
ANGAD KUMAR <159620549+ANGADKUMAR1111@users.noreply.github.com>
2025-08-10 23:05:12 +0530

    Update User.js
    password

--- models/User.js
+++ models/User.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema(
     bio: { type: String },
     followers: { type: Number, default: 0 },
     following: { type: Number, default: 0 },
+     password: { type: String, required: true },
   },
   { timestamps: true }
 );
